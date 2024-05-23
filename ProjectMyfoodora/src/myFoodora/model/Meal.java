a2f0d9e8503df41ad01008698acd982b8319c0a8
Fondation <teddytonin20003@gmail.com>
2024-05-24 00:01:48 +0200

    'Adding papyrusUML to the project. You might have to install additionnal dependency on your papyrus software'

--- ProjectMyfoodora/src/myFoodora/model/Meal.java
+++ ProjectMyfoodora/src/myFoodora/model/Meal.java
@@ -1,16 +1,31 @@
 package myFoodora.model;
 
 public class Meal {
+	private String name;
 	private double price;
 	private String category;
 	private boolean is_meal_of_the_week = false;
 	private Dish item1;
 	private Dish item2;
+	
+	public Meal(String category, Dish item1, Dish item2, String name) {
+		super();
+		this.category = category;
+		this.item1 = item1;
+		this.item2 = item2;
+		this.name = name;
+	}
 	public Meal(String category, Dish item1, Dish item2) {
 		super();
 		this.category = category;
 		this.item1 = item1;
 		this.item2 = item2;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName() {
+		this.name = name;
 	}
 	public double getPrice() {
 		return price;
@@ -24,7 +39,7 @@ public class Meal {
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	public boolean isIs_meal_of_the_week() {
+	public boolean Is_meal_of_the_week() {
 		return is_meal_of_the_week;
 	}
 	public void setIs_meal_of_the_week(boolean is_meal_of_the_week) {
