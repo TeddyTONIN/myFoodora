5f1d60f80201a0d4456cd5c946894e186d735a62
Jonathan_Tchokonte <jonathan.tchokonte-ngeutcheu@student-cs.fr>
2024-05-31 20:52:42 +0200

    suppréssion de myFoodora.app et rajout de la bonne app

--- ProjectMyfoodora/src/myFoodora/model/UserInterface.java
+++ ProjectMyfoodora/src/myFoodora/model/UserInterface.java
@@ -5,15 +5,7 @@ import java.util.Scanner;
 import myFoodora.design.abstractFactory.UserFactory;
 
 public class UserInterface {
-	
-	public void setup(String restaurants) { 
-		Scanner scanner=new Scanner(System.in);
-		UserFactory userFactory=new UserFactory();
-		int numRestaurants = Integer.parseInt(restaurants);
-		for (int i=1; i<=numRestaurants;i++) {
-			System.out.println("Quel est le nom du restaurant N"+i+" ?");
-			String restaurantName=scanner.nextLine();
-			MyFoodoraSystem.getInstance().getRestaurantsList().put(restaurantName, (Restaurant) userFactory.createUser("restaurant", restaurantName));		
-		}
+	public void registerRestaurant(String name,double x,double y) {
+		
 	}
 }
